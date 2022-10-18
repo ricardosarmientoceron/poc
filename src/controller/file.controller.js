a7b3a13aee8cfecf634d064dd073f1a281f7f544
rsarmiento.externo <ricardosarmientoceron@gmail.com>
2022-10-18 11:27:38 -0300

    first commit

---
+++ src/controller/file.controller.js
@@ -0,0 +1,96 @@
+
+const { validateRUT, getCheckDigit, generateRandomRUT } = require('validar-rut');
+const uploadFile = require("../middleware/upload");
+const fs = require("fs");
+const baseUrl = "http://localhost:8080/files/";
+const crypto = require('crypto');
+
+
+const upload = async (req, res) => {
+    try {
+        await uploadFile(req, res);
+
+        if (req.file == undefined) {
+            return res.status(400).send({ message: "Please upload a file!" });
+        }
+
+        if (validateRUT(req.body.rut+"-"+req.body.dv)!=true) {
+            return res.status(400).send({ message: "rut invalido" });
+        }
+
+        if (getCheckDigit(req.body.rut) != req.body.dv) {
+           return res.status(400).send({ message: "dv invalido" });
+        }
+
+      //  const fileBuffer = fs.readFileSync(req.file);
+      //  const hashSum = crypto.createHash('sha256');
+     //   hashSum.update(fileBuffer);
+
+      //  const hex = hashSum.digest('hex');
+
+//console.log(hex);
+
+        var hash = crypto.createHash('sha1');
+        //passing the data to be hashed
+        data = hash.update(req.file.originalname, 'utf-8');
+        //Creating the hash in the required format
+        gen_hash = data.digest('hex');
+        //Printing the output on the console
+        console.log("hash : " + gen_hash);
+
+
+
+        res.status(200).send({
+            message: "Uploaded the file successfully: " + req.file.originalname,
+            rut: req.body.rut,
+            dv: req.body.dv,
+            hash1: gen_hash
+        });
+    } catch (err) {
+        res.status(500).send({
+            message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+        });
+    }
+};
+
+const getListFiles = (req, res) => {
+    const directoryPath = __basedir + "/resources/static/assets/uploads/";
+
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            res.status(500).send({
+                message: "Unable to scan files!",
+            });
+        }
+
+        let fileInfos = [];
+
+        files.forEach((file) => {
+            fileInfos.push({
+                name: file,
+                url: baseUrl + file,
+            });
+        });
+
+        res.status(200).send(fileInfos);
+    });
+};
+
+const download = (req, res) => {
+    const fileName = req.params.name;
+    const directoryPath = __basedir + "/resources/static/assets/uploads/";
+
+    res.download(directoryPath + fileName, fileName, (err) => {
+        if (err) {
+            res.status(500).send({
+                message: "Could not download the file. " + err,
+            });
+        }
+    });
+};
+
+module.exports = {
+    upload,
+    getListFiles,
+    download,
+};
---
+++ src/middleware/upload.js
@@ -0,0 +1,29 @@
+const util = require("util");
+const multer = require("multer");
+const maxSize = 2 * 1024 * 1024;
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, __basedir + "/resources/static/assets/uploads/");
+    },
+    filename: (req, file, cb) => {
+        console.log(file.originalname);
+        cb(null, file.originalname);
+    },
+
+  //  rut: (req, rut, cb) => {
+  //      console.log(req.rut);
+  //      cb(null,req.rut);
+ //   },
+
+
+
+});
+
+let uploadFile = multer({
+    storage: storage,
+    limits: { fileSize: maxSize },
+}).single("file");
+
+let uploadFileMiddleware = util.promisify(uploadFile);
+module.exports = uploadFileMiddleware;
